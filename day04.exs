f4d46bc0e7ceae6e1075fb5fcff674eeeef232b4
Adrian Borup <adrian@borup.com>
2022-12-04 14:31:50 +0100

    Add day 4 part 2

--- day04.exs
+++ day04.exs
@@ -1,10 +1,14 @@
 defmodule Part1 do
   def solve() do
-    File.stream!("inputs/day04-input.txt")
-    |> Stream.map(&parse_line/1)
+    read_lines()
     |> Stream.filter(&is_contained?/1)
     |> Enum.count()
     |> IO.inspect()
+  end
+
+  def read_lines() do
+    File.stream!("inputs/day04-input.txt")
+    |> Stream.map(&parse_line/1)
   end
 
   def parse_line(line) do
@@ -18,4 +22,18 @@ defmodule Part1 do
   end
 end
 
+defmodule Part2 do
+  def solve() do
+    Part1.read_lines()
+    |> Stream.filter(&overlaps?/1)
+    |> Enum.count()
+    |> IO.inspect()
+  end
+
+  def overlaps?([a, b, c, d]) do
+    a <= d and c <= b
+  end
+end
+
 Part1.solve()
+Part2.solve()
