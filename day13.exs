07da1a69124add015f442df9d98def14db8b4211
Adrian Borup <adrian@borup.com>
2022-12-14 21:26:10 +0100

    Add day 13 part 1

---
+++ day13.exs
@@ -0,0 +1,60 @@
+defmodule Part1 do
+  def solve() do
+    read_input()
+    |> Stream.with_index(1)
+    |> Stream.filter(fn {pair, _} -> compare(pair) == :valid end)
+    |> Stream.map(&elem(&1, 1))
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+
+  def compare(pair) do
+    case pair do
+      {left, right} when is_integer(left) and is_integer(right) ->
+        cond do
+          left < right -> :valid
+          left > right -> :invalid
+          left == right -> :unsure
+        end
+
+      {left, right} when is_list(left) and is_list(right) ->
+        compare_lists(left, right)
+
+      {left, right} when is_integer(left) and is_list(right) ->
+        compare({[left], right})
+
+      {left, right} when is_list(left) and is_integer(right) ->
+        compare({left, [right]})
+    end
+  end
+
+  def compare_lists(left, right) do
+    case {left, right} do
+      {[], []} ->
+        :unsure
+
+      {[], [_ | _]} ->
+        :valid
+
+      {[_ | _], []} ->
+        :invalid
+
+      {[l_val | l_tail], [r_val | r_tail]} ->
+        case compare({l_val, r_val}) do
+          :unsure -> compare_lists(l_tail, r_tail)
+          res -> res
+        end
+    end
+  end
+
+  def read_input() do
+    File.stream!("inputs/day13-input.txt")
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.reject(&(String.length(&1) == 0))
+    |> Stream.map(&(Code.eval_string(&1) |> elem(0)))
+    |> Stream.chunk_every(2)
+    |> Stream.map(fn [left, right] -> {left, right} end)
+  end
+end
+
+Part1.solve()
