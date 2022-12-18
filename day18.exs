3e42625bd09750e6ac8434eb9044f12a2bbb7e96
Adrian Borup <adrian@borup.com>
2022-12-18 13:42:24 +0100

    Add day 18 part 2

--- day18.exs
+++ day18.exs
@@ -1,17 +1,18 @@
 defmodule Part1 do
   def solve() do
-    read_input()
+    Shared.read_input()
     |> get_surface_area()
-    |> IO.inspect()
   end
 
   def get_surface_area(lava_blocks) do
     lava_blocks
-    |> Enum.map(&count_lava_neighbours(lava_blocks, &1))
+    |> Enum.map(&Shared.count_lava_neighbours(lava_blocks, &1))
     |> Enum.map(&(6 - &1))
     |> Enum.sum()
   end
+end
 
+defmodule Shared do
   def count_lava_neighbours(lava_blocks, block) do
     neighbours(block)
     |> Enum.filter(&Enum.member?(lava_blocks, &1))
@@ -42,4 +43,51 @@ defmodule Part1 do
   end
 end
 
-Part1.solve()
+defmodule Part2 do
+  def solve() do
+    Shared.read_input()
+    |> get_exterior_surface_area()
+  end
+
+  # Uses flood fill: https://en.wikipedia.org/wiki/Flood_fill
+  def get_exterior_surface_area(lava_blocks) do
+    outside_coord = get_outside_coordinate(lava_blocks)
+    steam_blocks = flood_fill(lava_blocks, outside_coord)
+
+    steam_blocks
+    |> Enum.map(&Shared.count_lava_neighbours(lava_blocks, &1))
+    |> Enum.sum()
+  end
+
+  def flood_fill(lava_blocks, coordinate, visited \\ MapSet.new()) do
+    visited = MapSet.put(visited, coordinate)
+
+    Shared.neighbours(coordinate)
+    |> Enum.reject(&MapSet.member?(lava_blocks, &1))
+    |> Enum.reject(&MapSet.member?(visited, &1))
+    |> Enum.filter(&is_close_to_lava(lava_blocks, &1))
+    |> Enum.reduce(visited, fn neighbour, visited ->
+      flood_fill(lava_blocks, neighbour, visited)
+    end)
+  end
+
+  def is_close_to_lava(lava_blocks, coordinate) do
+    has_lava_neighbour(lava_blocks, coordinate) or
+      Enum.any?(Shared.neighbours(coordinate), &has_lava_neighbour(lava_blocks, &1))
+  end
+
+  def has_lava_neighbour(lava_blocks, coordinate) do
+    Shared.neighbours(coordinate) |> Enum.any?(&MapSet.member?(lava_blocks, &1))
+  end
+
+  def get_outside_coordinate(lava_blocks) do
+    {x, y, z} = Enum.min(lava_blocks)
+    {x - 1, y, z}
+  end
+end
+
+{part1_time, part1} = :timer.tc(&Part1.solve/0)
+{part2_time, part2} = :timer.tc(&Part2.solve/0)
+
+IO.puts("Part 1: #{part1} (#{part1_time / 1000}ms)")
+IO.puts("Part 2: #{part2} (#{part2_time / 1000}ms)")
