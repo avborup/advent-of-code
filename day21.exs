96891510c5e4f0678e44520fdfd4a96817131489
Adrian Borup <adrian@borup.com>
2022-12-21 17:21:53 +0100

    Add day 21 part 2

--- day21.exs
+++ day21.exs
@@ -43,4 +43,65 @@ defmodule Part1 do
   end
 end
 
+defmodule Part2 do
+  def solve() do
+    Part1.read_input()
+    |> find_variable_value()
+    |> IO.inspect()
+  end
+
+  def find_variable_value(monkeys) do
+    {first, _, second} = Map.get(monkeys, "root")
+
+    {[first_monkey | subpath], expected} =
+      case {find_humn_path(monkeys, first), find_humn_path(monkeys, second)} do
+        {path, nil} -> {path, Part1.calculate_yell(monkeys, second)}
+        {nil, path} -> {path, Part1.calculate_yell(monkeys, first)}
+      end
+
+    solve_equation(monkeys, first_monkey, subpath, expected)
+  end
+
+  def solve_equation(_, _, [], expected) do
+    expected
+  end
+
+  def solve_equation(monkeys, monkey, [next | path], expected) do
+    {first, op, second} = Map.get(monkeys, monkey)
+    is_left = first == next
+    other_monkey = if(is_left, do: second, else: first)
+    other = Part1.calculate_yell(monkeys, other_monkey)
+
+    new_expected =
+      case op do
+        "+" -> expected - other
+        "-" when is_left -> other + expected
+        "-" -> other - expected
+        "*" -> expected / other
+        "/" -> expected * other
+      end
+
+    solve_equation(monkeys, next, path, new_expected)
+  end
+
+  def find_humn_path(_, "humn") do
+    ["humn"]
+  end
+
+  def find_humn_path(monkeys, monkey) do
+    case Map.get(monkeys, monkey) do
+      num when is_integer(num) ->
+        nil
+
+      {first, _, second} ->
+        case {find_humn_path(monkeys, first), find_humn_path(monkeys, second)} do
+          {nil, nil} -> nil
+          {first_res, nil} -> [monkey | first_res]
+          {nil, second_res} -> [monkey | second_res]
+        end
+    end
+  end
+end
+
 Part1.solve()
+Part2.solve()
