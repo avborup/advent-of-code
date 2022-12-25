fb19caf558e0385bbc68a964ac31c360133405c6
Adrian Borup <adrian@borup.com>
2022-12-25 23:24:31 +0100

    Add day 11 part 2

--- day11.exs
+++ day11.exs
@@ -5,14 +5,31 @@ defmodule Monkey do
             test: nil,
             true_target: nil,
             false_target: nil,
+            dividend: nil,
             inspect_count: 0
 end
 
-defmodule Part1 do
-  def solve() do
-    Parsing.read_input()
-    |> simulate_rounds()
-    |> Enum.at(20)
+defmodule Solution do
+  def solve(part) do
+    monkeys = Parsing.read_input()
+
+    round =
+      case part do
+        :part1 -> 20
+        :part2 -> 10000
+      end
+
+    lcm = find_lcm(monkeys)
+
+    lose_interest =
+      case part do
+        :part1 -> fn n -> div(n, 3) end
+        :part2 -> fn n -> rem(n, lcm) end
+      end
+
+    monkeys
+    |> simulate_rounds(lose_interest)
+    |> Enum.at(round)
     |> Enum.map(fn {_, monkey} -> monkey.inspect_count end)
     |> Enum.sort(:desc)
     |> Enum.take(2)
@@ -20,18 +37,18 @@ defmodule Part1 do
     |> IO.inspect()
   end
 
-  defp simulate_rounds(monkeys) do
+  defp simulate_rounds(monkeys, lose_interest) do
     Stream.unfold(monkeys, fn monkeys ->
-      {monkeys, simulate_round(monkeys)}
+      {monkeys, simulate_round(monkeys, lose_interest)}
     end)
   end
 
-  defp simulate_round(monkeys) do
+  defp simulate_round(monkeys, lose_interest) do
     Map.keys(monkeys)
     |> Enum.reduce(monkeys, fn id, monkeys ->
       monkey = Map.get(monkeys, id)
 
-      {new_monkey, throws} = simulate_turn(monkey)
+      {new_monkey, throws} = simulate_turn(monkey, lose_interest)
 
       monkeys = Map.put(monkeys, new_monkey.id, new_monkey)
 
@@ -47,12 +64,12 @@ defmodule Part1 do
     end)
   end
 
-  defp simulate_turn(monkey) do
+  defp simulate_turn(monkey, lose_interest) do
     {new_monkey, throws} =
       Enum.reduce(monkey.items, {monkey, []}, fn _, {monkey, throws} ->
         [item | items] = monkey.items
 
-        worry = monkey.operation.(item) |> div(3)
+        worry = monkey.operation.(item) |> lose_interest.()
 
         target =
           case monkey.test.(worry) do
@@ -66,6 +83,12 @@ defmodule Part1 do
       end)
 
     {new_monkey, Enum.reverse(throws)}
+  end
+
+  # All the dividends are small primes - so we essentially got prime factorization for free
+  defp find_lcm(monkeys) do
+    Enum.map(monkeys, fn {_, monkey} -> monkey.dividend end)
+    |> Enum.reduce(1, &(&1 * &2))
   end
 end
 
@@ -82,11 +105,14 @@ defmodule Parsing do
   defp parse_monkey(chunk) do
     [id_line, items_line, op_line, test_line, true_line, false_line] = chunk
 
+    dividend = parse_dividend(test_line)
+
     %Monkey{
       id: parse_id(id_line),
       items: parse_items(items_line),
       operation: parse_operation(op_line),
-      test: parse_test(test_line),
+      test: fn n -> rem(n, dividend) == 0 end,
+      dividend: dividend,
       true_target: parse_throw(true_line),
       false_target: parse_throw(false_line)
     }
@@ -128,11 +154,9 @@ defmodule Parsing do
     end
   end
 
-  defp parse_test(test_line) do
-    [_, num] = String.split(test_line, "by ")
-    num = String.to_integer(num)
-
-    fn n -> rem(n, num) == 0 end
+  defp parse_dividend(test_line) do
+    [_, dividend] = String.split(test_line, "by ")
+    String.to_integer(dividend)
   end
 
   defp parse_throw(throw_line) do
@@ -141,4 +165,5 @@ defmodule Parsing do
   end
 end
 
-Part1.solve()
+Solution.solve(:part1)
+Solution.solve(:part2)
