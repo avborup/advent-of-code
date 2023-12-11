09c6f1a57e55f2821e4c6415be372013c679da04
Adrian Borup <adrian@borup.com>
2023-12-11 07:29:03 +0100

    Clean up day 11 sliiightly

--- 2023/day11.scala
+++ 2023/day11.scala
@@ -1,11 +1,9 @@
 package avborup.aoc2023.day11
 
-import scala.io.Source.fromFile
-
 object Day11 {
   def main(args: Array[String]) =
     val file = args.lift(0).getOrElse("inputs/day11.txt")
-    val input = fromFile(file).mkString
+    val input = scala.io.Source.fromFile(file).mkString
 
     println(s"Part 1: ${part1(input)}")
     println(s"Part 2: ${part2(input)}")
@@ -17,9 +15,9 @@ object Day11 {
     Input.parse(input, 1_000_000).summedDistances
 }
 
-case class Input(galaxies: Set[Coord]) {
+extension (galaxies: List[Coord]) {
   def summedDistances =
-    galaxies.toList
+    galaxies
       .combinations(2)
       .map { case List((r1, c1), (r2, c2)) =>
         Math.abs(r1 - r2) + Math.abs(c1 - c2)
@@ -37,21 +35,20 @@ object Input {
     val emptyRows = rows.zipWithIndex.filter(_._1.forall(_ == '.')).map(_._2)
     val emptyCols = cols.zipWithIndex.filter(_._1.forall(_ == '.')).map(_._2)
 
-    val galaxies = input.linesIterator.zipWithIndex
-      .foldLeft((Set[Coord](), 0)) { case ((galaxies, r), (line, i)) =>
+    val (galaxies, _) = input.linesIterator.zipWithIndex
+      .foldLeft((List[Coord](), 0)) { case ((galaxies, r), (line, i)) =>
         if emptyRows.contains(i) then (galaxies, r + expansionSize)
         else
-          val cols = line.zipWithIndex.foldLeft((galaxies, 0)) {
+          val (galaxiesAcc, _) = line.zipWithIndex.foldLeft((galaxies, 0)) {
             case ((galaxies, c), (symbol, j)) =>
               symbol match
                 case _ if emptyCols.contains(j) => (galaxies, c + expansionSize)
                 case '.'                        => (galaxies, c + 1)
-                case s                          => (galaxies + ((r, c)), c + 1)
+                case s                          => ((r, c) :: galaxies, c + 1)
           }
 
-          (cols._1, r + 1)
+          (galaxiesAcc, r + 1)
       }
-      ._1
 
-    Input(galaxies)
+    galaxies
 }
