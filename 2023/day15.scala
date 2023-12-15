52fd3a1aac95ba97ead851756f7352412c68ab28
Adrian Borup <adrian@borup.com>
2023-12-15 08:00:00 +0100

    Add day 15, part 2

--- 2023/day15.scala
+++ 2023/day15.scala
@@ -1,5 +1,7 @@
 package avborup.aoc2023.day15
 
+import scala.collection.mutable.ListBuffer
+
 object Day15 {
   def main(args: Array[String]) =
     val input = Input.read()
@@ -10,7 +12,37 @@ object Day15 {
     seq.map(_.HASH).sum
 
   def part2(seq: List[String]) =
-    ()
+    var boxes = Array.fill(256)(ListBuffer[(String, Int)]())
+
+    def add(label: String, i: Int) =
+      val hash = label.HASH
+      boxes(hash).map(_._1).indexOf(label) match
+        case -1 => boxes(hash).append((label, i))
+        case j  => boxes(hash).update(j, (label, i))
+
+    def remove(label: String) =
+      val hash = label.HASH
+      boxes(hash).map(_._1).indexOf(label) match
+        case -1 => ()
+        case j  => boxes(hash).remove(j)
+
+    seq.foreach({
+      case op if op.contains("=") => {
+        val Array(label, value) = op.split("=")
+        add(label, value.toInt)
+      }
+      case op if op.contains("-") =>
+        val Array(label) = op.split("-")
+        remove(label)
+    })
+
+    boxes.zipWithIndex
+      .flatMap({ case (box, boxNumber) =>
+        box.zipWithIndex.map({ case ((label, l), i) =>
+          (boxNumber + 1) * (i + 1) * l
+        })
+      })
+      .sum
 }
 
 extension (s: String) {
