14f9b0429ffb8a38035eab5f1c98293feeddb020
Adrian Borup <adrian@borup.com>
2024-10-28 09:48:43 +0100

    Add 2021, day 5 part 2

--- 2021/day05.pl
+++ 2021/day05.pl
@@ -5,29 +5,34 @@ use Data::Dumper;
 use List::Util qw(min max sum);
 
 sub part_1 {
-  my @input = @_;
-  my @horizontal = grep { $_->[0] eq $_->[2] || $_->[1] eq $_->[3] } @input;
+  my @horizontal_vertical = grep { $_->[0] eq $_->[2] || $_->[1] eq $_->[3] } @_;
+  return count_intersections(@horizontal_vertical);
+}
 
-  my $w = max(map { max($_->[0], $_->[2]) } @horizontal);
-  my $h = max(map { max($_->[1], $_->[3]) } @horizontal);
+sub part_2 {
+  return count_intersections(@_);
+}
+
+sub count_intersections {
+  my @lines = @_;
+
+  my $w = max(map { max($_->[0], $_->[2]) } @lines);
+  my $h = max(map { max($_->[1], $_->[3]) } @lines);
 
   my @grid = map { [(0) x ($w + 1)] } (0..$h);
 
-  for my $line (@horizontal) {
+  for my $line (@lines) {
     my ($x1, $y1, $x2, $y2) = @$line;
 
-    for my $x (min($x1, $x2)..max($x1, $x2)) {
-      for my $y (min($y1, $y2)..max($y1, $y2)) {
-        $grid[$y][$x] += 1;
-      }
+    my ($xd, $yd) = ($x2 - $x1, $y2 - $y1);
+    for my $i (0..max(abs($xd), abs($yd))) {
+      my $x = $x1 + (($xd <=> 0) * $i);
+      my $y = $y1 + (($yd <=> 0) * $i);
+      $grid[$y][$x] += 1;
     }
   }
 
   return sum(map { scalar grep { $_ > 1 } @$_ } @grid);
-}
-
-sub part_2 {
-  "?"
 }
 
 sub read_input {
