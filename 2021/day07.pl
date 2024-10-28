544c2d4851f27089c192098a936a437c4525bc30
Adrian Borup <adrian@borup.com>
2024-10-28 11:39:30 +0100

    Add 2021, day 7 part 2

--- 2021/day07.pl
+++ 2021/day07.pl
@@ -5,19 +5,34 @@ use Data::Dumper;
 use List::Util qw(min max sum);
 
 sub part_1 {
-  my @crabs = @_;
+  my $cost_function = sub {
+    my ($crab, $target) = @_;
+    return abs($crab - $target);
+  };
+
+  return find_min_cost(\@_, $cost_function);
+}
+
+sub part_2 {
+  my $cost_function = sub {
+    my ($crab, $target) = @_;
+    my $steps = abs($crab - $target);
+    return $steps * ($steps + 1) / 2
+  };
+
+  return find_min_cost(\@_, $cost_function);
+}
+
+sub find_min_cost {
+  my ($crabs, $cost_function) = @_;
 
   my $min_fuel = "inf";
-  for my $target (min(@crabs) .. max(@crabs)) {
-    my $fuel = sum(map { abs($_ - $target) } @crabs);
+  for my $target (min(@$crabs) .. max(@$crabs)) {
+    my $fuel = sum(map { $cost_function->($_, $target) } @$crabs);
     $min_fuel = min($min_fuel, $fuel);
   }
 
   return $min_fuel;
-}
-
-sub part_2 {
-  "?"
 }
 
 my @input = map { /\d+/g } <>;
