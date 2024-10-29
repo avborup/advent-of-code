de1f6d713df51f9a66e1c9500b061d54f260a0b4
Adrian Borup <adrian@borup.com>
2024-10-29 14:19:37 +0100

    Use complex numbers instead of 2D array

--- 2021/day09.pl
+++ 2021/day09.pl
@@ -8,7 +8,7 @@ use Data::Dumper;
 sub part_1 {
   my ($matrix) = @_;
   my @lows = find_low_points($matrix);
-  return sum(map { 1 + $matrix->[$_->[0]][$_->[1]] } @lows);
+  return sum(map { 1 + $matrix->{$_} } @lows);
 }
 
 sub part_2 {
@@ -16,26 +16,18 @@ sub part_2 {
   my @lows = find_low_points($matrix);
 
   my %visited;
-
   my $dfs = sub {
     my ($start) = @_;
-    my ($sr, $sc) = @$start;
-
     my @stack = ($start);
-    $visited{cplx($sc, $sr)} = 1;
-    my $size = 1;
+    $visited{$start} = 1;
 
+    my $size = 1;
     while (@stack) {
       my $v = pop @stack;
-      my ($vr, $vc) = @$v;
 
-      foreach my $adj (@{neighbours($vr, $vc, $matrix)}) {
-        my ($ar, $ac) = @$adj;
-
-        next if $visited{cplx($ac, $ar)};
-        $visited{cplx($ac, $ar)} = 1;
-
-        next unless $matrix->[$ar][$ac] < 9;
+      foreach my $adj (@{neighbours($v, $matrix)}) {
+        next if $visited{$adj} || $matrix->{$adj} >= 9;
+        $visited{$adj} = 1;
 
         $size++;
         push @stack, $adj;
@@ -53,26 +45,60 @@ sub part_2 {
 sub find_low_points {
   my ($matrix) = @_;
   my @lows;
-  for my $r (0 .. $#$matrix) {
-      for my $c (0 .. $#{$matrix->[$r]}) {
-      my $height = $matrix->[$r][$c];
-      my $adj = neighbours($r, $c, $matrix);
-      push @lows, [$r, $c] if all { $height < $matrix->[$_->[0]][$_->[1]] } @$adj;
-    }
+  for my $v (map { cp($_) } keys %$matrix) {
+    push @lows, $v if all { $matrix->{$v} < $matrix->{$_} } @{neighbours($v, $matrix)};
   }
   return @lows;
 }
 
 sub neighbours {
-  my ($x, $y, $matrix) = @_;
-  my ($R, $C, @neigbours) = ($#$matrix, $#{$matrix->[0]});
-  foreach my $dir ([-1, 0], [1, 0], [0, -1], [0, 1]) {
-    my ($nx, $ny) = ($x + $dir->[0], $y + $dir->[1]);
-    push @neigbours, [$nx, $ny] unless $nx < 0 || $nx > $R || $ny < 0 || $ny > $C;
+  my ($v, $matrix) = @_;
+  my @neighbours;
+  foreach my $dir (cplx(-1, 0), cplx(1, 0), cplx(0, -1), cplx(0, 1)) {
+    my $n = $v + $dir;
+    push @neighbours, $n if exists $matrix->{$n};
   }
-  return \@neigbours;
+  return \@neighbours;
 }
 
-my @matrix = map { [split //] } map { chomp; $_ } <>;
-print("Part 1: ", part_1(\@matrix), "\n");
-print("Part 2: ", part_2(\@matrix), "\n");
+# Bug in Perl? I've reported it here: https://github.com/Perl/perl5/issues/22711
+# DB<1> p cplx(2, 1)
+#   2+i
+# DB<2> p cplx("2+i")
+#   0   <- wrong!
+# DB<3> p cplx("2+1i") <- but this works
+#   2+i  
+sub cp {
+  my ($str) = @_;
+  $str =~ /^(-?\d+)?([+-]?)(\d*)(i?)$/;
+
+  my ($re, $im);
+  if ($4 && not $3) {
+    $re = 0;
+    $im = $1 || 0;
+  } else {
+    $re = $1 || 0;
+    $im = $4 ? ($3 || 1) : 0;
+  }
+  $im *= -1 if $2 eq '-';
+
+  return cplx($re, $im);
+}
+
+sub parse_input {
+  my @matrix = map { [split //] } map { chomp; $_ } <>;
+  my ($R, $C) = ($#matrix, $#{$matrix[0]});
+
+  my %coords;
+  for my $r (0 .. $R) {
+    for my $c (0 .. $C) {
+      $coords{cplx($r, $c)} = $matrix[$r][$c];
+    }
+  }
+
+  return (\%coords, $R, $C);
+}
+
+my ($matrix) = parse_input();
+print("Part 1: ", part_1($matrix), "\n");
+print("Part 2: ", part_2($matrix), "\n");
