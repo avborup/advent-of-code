ed8806fcb017654f78423f4793a31fa94d2c92a0
Adrian Borup <adrian@borup.com>
2024-10-29 14:14:27 +0100

    Add 2021, day 9 part 2

--- 2021/day09.pl
+++ 2021/day09.pl
@@ -1,26 +1,66 @@
 #!/usr/bin/env perl -w
 use strict;
 
-use List::Util qw(all sum);
+use List::Util qw(all sum product);
+use Math::Complex;
 use Data::Dumper;
 
 sub part_1 {
-  my @matrix = map { [split //] } @_;
-
-  my @lows;
-  for my $r (0 .. $#matrix) {
-    for my $c (0 .. $#{$matrix[$r]}) {
-      my $height = $matrix[$r][$c];
-      my $adj = neighbours($r, $c, \@matrix);
-      push @lows, [$r, $c] if all { $height < $matrix[$_->[0]][$_->[1]] } @$adj;
-    }
-  }
-
-  return sum(map { 1 + $matrix[$_->[0]][$_->[1]] } @lows);
+  my ($matrix) = @_;
+  my @lows = find_low_points($matrix);
+  return sum(map { 1 + $matrix->[$_->[0]][$_->[1]] } @lows);
 }
 
 sub part_2 {
-  "?"
+  my ($matrix) = @_;
+  my @lows = find_low_points($matrix);
+
+  my %visited;
+
+  my $dfs = sub {
+    my ($start) = @_;
+    my ($sr, $sc) = @$start;
+
+    my @stack = ($start);
+    $visited{cplx($sc, $sr)} = 1;
+    my $size = 1;
+
+    while (@stack) {
+      my $v = pop @stack;
+      my ($vr, $vc) = @$v;
+
+      foreach my $adj (@{neighbours($vr, $vc, $matrix)}) {
+        my ($ar, $ac) = @$adj;
+
+        next if $visited{cplx($ac, $ar)};
+        $visited{cplx($ac, $ar)} = 1;
+
+        next unless $matrix->[$ar][$ac] < 9;
+
+        $size++;
+        push @stack, $adj;
+      }
+    }
+
+    return $size;
+  };
+
+  my @sizes = map { $dfs->($_) } @lows;
+
+  return product((sort { $b <=> $a } @sizes)[0..2]);
+}
+
+sub find_low_points {
+  my ($matrix) = @_;
+  my @lows;
+  for my $r (0 .. $#$matrix) {
+      for my $c (0 .. $#{$matrix->[$r]}) {
+      my $height = $matrix->[$r][$c];
+      my $adj = neighbours($r, $c, $matrix);
+      push @lows, [$r, $c] if all { $height < $matrix->[$_->[0]][$_->[1]] } @$adj;
+    }
+  }
+  return @lows;
 }
 
 sub neighbours {
@@ -33,6 +73,6 @@ sub neighbours {
   return \@neigbours;
 }
 
-chomp(my @input = <>);
-print("Part 1: ", part_1(@input), "\n");
-print("Part 2: ", part_2(@input), "\n");
+my @matrix = map { [split //] } map { chomp; $_ } <>;
+print("Part 1: ", part_1(\@matrix), "\n");
+print("Part 2: ", part_2(\@matrix), "\n");
