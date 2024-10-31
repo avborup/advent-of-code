f6fd4f4a8667678327405ca6f564ca45c528af56
Adrian Borup <adrian@borup.com>
2024-10-31 21:54:39 +0100

    Add 2021, day 11 part 1

---
+++ 2021/day11.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl -w
+use strict;
+
+use Data::Dumper;
+use List::Util qw(sum);
+
+sub part_1 {
+  my @matrix = map { [split //] } @_;
+  return sum(map { simulate_step(\@matrix) } 1..100);
+}
+
+sub part_2 {
+  "?"
+}
+
+sub simulate_step {
+  my ($matrix) = @_;
+  my ($R, $C) = ($#{$matrix}, $#{$matrix->[0]});
+
+  for my $r (0..$R) {
+    for my $c (0..$C) {
+      $matrix->[$r][$c]++;
+    }
+  }
+
+  my @flash_queue;
+  for my $r (0..$R) {
+    for my $c (0..$C) {
+      if ($matrix->[$r][$c] > 9) {
+        push @flash_queue, [$r, $c];
+      }
+    }
+  }
+
+  my @flashed = map { [(0) x $C] } 0..$R;
+  my $num_flashed = 0;
+  while (@flash_queue) {
+    my ($r, $c) = @{shift @flash_queue};
+    next if $flashed[$r][$c];
+
+    $matrix->[$r][$c] = 0;
+    $flashed[$r][$c] = 1;
+    $num_flashed++;
+
+    foreach my $dir ([-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]) {
+      my ($nr, $nc) = ($r + $dir->[0], $c + $dir->[1]);
+
+      next if $nr < 0 || $nr > $R || $nc < 0 || $nc > $C;
+      next if $flashed[$nr][$nc];
+
+      $matrix->[$nr][$nc]++;
+      push @flash_queue, [$nr, $nc] if $matrix->[$nr][$nc] > 9;
+    }
+  }
+
+  return $num_flashed;
+}
+
+chomp(my @input = <>);
+print("Part 1: ", part_1(@input), "\n");
+print("Part 2: ", part_2(@input), "\n");
