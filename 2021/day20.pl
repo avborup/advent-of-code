ef0922c98a1a9b60aa9a2e471fc91b74cb55617e
Adrian Borup <adrian@borup.com>
2024-11-12 09:38:53 +0100

    Slightly clean up 2021, day 20

--- 2021/day20.pl
+++ 2021/day20.pl
@@ -19,13 +19,6 @@ sub solve_with_n {
   return $count;
 }
 
-sub print_image {
-  my ($image) = @_;
-  for my $row (@$image) {
-    print(join("", map { $_ ? '#' : '.' } @$row), "\n");
-  }
-}
-
 sub enhance {
   my ($state) = @_;
 
@@ -33,22 +26,22 @@ sub enhance {
   my $alg = $state->{algorithm};
   my $default = $alg->[0] && !$alg->[-1] && $state->{step} % 2 == 1 ? 1 : 0;
 
-  my ($NR, $NC) = ($state->{R} + $padding * 2, $state->{C} + $padding * 2);
+  my ($R, $C) = (scalar @{$state->{image}}, scalar @{$state->{image}->[0]});
+  my ($NR, $NC) = ($R + $padding * 2, $C + $padding * 2);
   my $image = [map { [($default) x $NC] } 1..$NR];
   my $output = [map { [($default) x $NC] } 1..$NR];
 
-  for my $r (0..$state->{R} - 1) {
-    for my $c (0..$state->{C} - 1) {
+  for my $r (0..$R - 1) {
+    for my $c (0..$C - 1) {
       $image->[$r + $padding][$c + $padding] = $state->{image}[$r][$c];
     }
   }
 
-  for my $r ($padding-1..$state->{R} + $padding) {
-    for my $c ($padding-1..$state->{C} + $padding) {
+  for my $r (0..$NR-1) {
+    for my $c (0..$NC-1) {
       my $index = 0;
-      for my $dr (-1..1) {
-        for my $dc (-1..1) {
-          my ($tr, $tc) = ($r + $dr, $c + $dc);
+      for my $tr ($r-1..$r+1) {
+        for my $tc ($c-1..$c+1) {
           $index <<= 1;
           unless ($tr < 0 || $tr >= $NR || $tc < 0 || $tc >= $NC) {
             $index |= $image->[$tr][$tc];
@@ -65,9 +58,14 @@ sub enhance {
   return {
     algorithm => $state->{algorithm},
     image => $output,
-    R => $NR,
-    C => $NC,
     step => $state->{step} + 1,
+  }
+}
+
+sub print_image {
+  my ($image) = @_;
+  for my $row (@$image) {
+    print(join("", map { $_ ? '#' : '.' } @$row), "\n");
   }
 }
 
@@ -77,8 +75,6 @@ sub parse_input {
   return {
     algorithm => [map { $_ eq '#' } split //, $algorithm],
     image => [map { [map { $_ eq '#' } split //] } @lines],
-    R => scalar @lines,
-    C => length($lines[0]),
     step => 0,
   }
 }
