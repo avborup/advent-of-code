4c852fd21d5d1afc013c684c8cbad43cb38e8cf5
Adrian Borup <adrian@borup.com>
2024-11-29 15:29:46 +0100

    Add day-initialisation script for Perl to 2024

---
+++ 2024/init-day.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+use strict; use warnings;
+
+use File::Path qw(make_path);
+
+my $day = $ARGV[0] // die "Usage: $0 <day>\n";
+my $sol_file = "day$day.pl";
+
+open(my $fh, '>', $sol_file) or die $!;
+print $fh <<'EOF';
+#!/usr/bin/env perl -w
+use strict;
+
+use Data::Dumper;
+
+sub part_1 {
+  "?"
+}
+
+sub part_2 {
+  "?"
+}
+
+chomp(my @input = <>);
+print("Part 1: ", part_1(@input), "\n");
+print("Part 2: ", part_2(@input), "\n");
+EOF
+# Make solution file executable
+chmod(0755, $sol_file);
+
+make_path("inputs/$day");
+
+print "Initialised day $day - happy coding!\n";
