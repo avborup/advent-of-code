9e329599bdbd651d072035be9cc635ee3c65679d
Adrian Borup <adrian@borup.com>
2024-12-01 06:39:43 +0100

    Simplify day 1 structure a bit

--- 2024/day01.pl
+++ 2024/day01.pl
@@ -1,40 +1,23 @@
 #!/usr/bin/env perl -w
 use strict;
 
-use Data::Dumper;
 use List::Util qw(sum);
 
-sub part_1 {
-  my ($l1, $l2) = @_;
-
-  @$l1 = sort { $a <=> $b } @$l1;
-  @$l2 = sort { $a <=> $b } @$l2;
-
-  return sum(map { abs($l2->[$_] - $l1->[$_]) } 0..$#$l1);
+my @nums = join("", <>) =~ /-?\d+/g;
+my (@l1, @l2) = ();
+while (@nums) {
+  push(@l1, shift @nums);
+  push(@l2, shift @nums);
 }
 
-sub part_2 {
-  my ($l1, $l2) = @_;
+@l1 = sort { $a <=> $b } @l1;
+@l2 = sort { $a <=> $b } @l2;
 
-  my %scores;
-  for my $b (@$l2) {
-    $scores{$b} = 0 if !exists $scores{$b};
-    $scores{$b}++;
-  }
+my $part1 = sum(map { abs($l2[$_] - $l1[$_]) } 0..$#l1);
+print("Part 1: $part1\n");
 
-  return sum(map { $_ * ($scores{$_} // 0) } @$l1);
-}
+my %scores = map { $_ => 0 } @l2;
+$scores{$_}++ for @l2;
 
-sub read_input {
-  my @nums = join("", <>) =~ /-?\d+/g;
-  my (@l1, @l2) = ();
-  while (@nums) {
-    push(@l1, shift @nums);
-    push(@l2, shift @nums);
-  }
-  return (\@l1, \@l2);
-}
-
-my @input = read_input();
-print("Part 1: ", part_1(@input), "\n");
-print("Part 2: ", part_2(@input), "\n");
+my $part2 = sum(map { $_ * ($scores{$_} // 0) } @l1);
+print("Part 2: $part2\n");
