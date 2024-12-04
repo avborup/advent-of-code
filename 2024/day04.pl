48e2ed914b085b76fafca41ca8cc9fb66159d9f2
Adrian Borup <adrian@borup.com>
2024-12-04 06:23:52 +0100

    Add day 4, part 2
    have both

--- 2024/day04.pl
+++ 2024/day04.pl
@@ -3,14 +3,9 @@ use strict;
 
 use Data::Dumper;
 
-
 my $matrix = [map { chomp; [split //] } <>];
 
-my ($part1, $part2) = (0, 0);
-
-
-print Dumper($matrix);
-
+my $part1 = 0;
 my @pattern = split //, "XMAS";
 for my $r (0..$#{$matrix}) {
   for my $c (0..$#{$matrix->[$r]}) {
@@ -30,19 +25,53 @@ for my $r (0..$#{$matrix}) {
           last;
         }
 
-        print("nr: $nr, nc: $nc ", $matrix->[$nr][$nc], " ", $pattern[$s], "\n");
         if ($matrix->[$nr][$nc] ne $pattern[$s]) {
           $ok = 0;
           last;
         }
         ($nr, $nc) = ($nr + $dr, $nc + $dc);
       }
-      print "ok: $ok";
 
       if ($ok) {
         $part1++;
       }
-      print "\n"
+    }
+  }
+}
+
+my $part2 = 0;
+for my $r (0..$#{$matrix}) {
+  for my $c (0..$#{$matrix->[$r]}) {
+    if ($matrix->[$r][$c] ne "A") {
+      next;
+    }
+
+    my @pairs = (
+      [[-1, -1], [1, 1]],
+      [[-1, 1], [1, -1]],
+      [[1, 1], [-1, -1]],
+      [[1, -1], [-1, 1]],
+    );
+
+    my $matches = 0;
+    foreach my $pair (@pairs) {
+      my ($dir1, $dir2) = ($pair->[0], $pair->[1]);
+      my ($mdr, $mdc) = ($dir1->[0], $dir1->[1]);
+      my ($mr, $mc) = ($r + $mdr, $c + $mdc);
+      my ($sdr, $sdc) = ($dir2->[0], $dir2->[1]);
+      my ($sr, $sc) = ($r + $sdr, $c + $sdc);
+
+      if ($mr < 0 || $mr >= @{$matrix} || $mc < 0 || $mc >= @{$matrix->[$mr]} || $sr < 0 || $sr >= @{$matrix} || $sc < 0 || $sc >= @{$matrix->[$sr]}) {
+        next;
+      }
+
+      if ($matrix->[$mr][$mc] eq "M" && $matrix->[$sr][$sc] eq "S") {
+        $matches++;
+      }
+    }
+
+    if ($matches > 1) {
+      $part2++;
     }
   }
 }
