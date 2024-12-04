1a212790d6dca6544b1255b3e61a106a37750ea8
Adrian Borup <adrian@borup.com>
2024-12-04 08:27:14 +0100

    Clean up day 4

--- 2024/day04.pl
+++ 2024/day04.pl
@@ -4,47 +4,28 @@ use strict;
 use Data::Dumper;
 
 my $matrix = [map { chomp; [split //] } <>];
+my ($R, $C) = (scalar(@$matrix), scalar(@{$matrix->[0]}));
 
-my $part1 = 0;
-my @pattern = split //, "XMAS";
+my ($part1, @pattern) = (0, split(//, "XMAS"));
 for my $r (0..$#{$matrix}) {
   for my $c (0..$#{$matrix->[$r]}) {
-    if ($matrix->[$r][$c] ne "X") {
-      next;
-    }
-
     foreach my $dir ([-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]) {
-      my ($dr, $dc) = @$dir;
-      my ($nr, $nc) = ($r + $dr, $c + $dc);
-
-      my $ok = 1;
-
-      for my $s (1..$#pattern) {
-        if ($nr < 0 || $nr > $#$matrix || $nc < 0 || $nc > $#{$matrix->[$r]}) {
-          $ok = 0;
-          last;
-        }
-
-        if ($matrix->[$nr][$nc] ne $pattern[$s]) {
-          $ok = 0;
-          last;
-        }
-        ($nr, $nc) = ($nr + $dr, $nc + $dc);
+      my $xmas = 1;
+      for my $s (0..$#pattern) {
+        my ($nr, $nc) = ($r + $s * $dir->[0], $c + $s * $dir->[1]);
+        $xmas = 0, last if $nr < 0 || $nr >= $R || $nc < 0 || $nc >= $C;
+        $xmas = 0, last if $matrix->[$nr][$nc] ne $pattern[$s];
       }
 
-      if ($ok) {
-        $part1++;
-      }
+      $part1++ if $xmas;
     }
   }
 }
 
 my $part2 = 0;
-for my $r (0..$#{$matrix}) {
-  for my $c (0..$#{$matrix->[$r]}) {
-    if ($matrix->[$r][$c] ne "A") {
-      next;
-    }
+for my $r (1..$#{$matrix}-1) {
+  for my $c (1..$#{$matrix->[$r]}-1) {
+    next if $matrix->[$r][$c] ne "A";
 
     my @pairs = (
       [[-1, -1], [1, 1]],
@@ -55,24 +36,12 @@ for my $r (0..$#{$matrix}) {
 
     my $matches = 0;
     foreach my $pair (@pairs) {
-      my ($dir1, $dir2) = ($pair->[0], $pair->[1]);
-      my ($mdr, $mdc) = ($dir1->[0], $dir1->[1]);
-      my ($mr, $mc) = ($r + $mdr, $c + $mdc);
-      my ($sdr, $sdc) = ($dir2->[0], $dir2->[1]);
-      my ($sr, $sc) = ($r + $sdr, $c + $sdc);
-
-      if ($mr < 0 || $mr >= @{$matrix} || $mc < 0 || $mc >= @{$matrix->[$mr]} || $sr < 0 || $sr >= @{$matrix} || $sc < 0 || $sc >= @{$matrix->[$sr]}) {
-        next;
-      }
-
-      if ($matrix->[$mr][$mc] eq "M" && $matrix->[$sr][$sc] eq "S") {
-        $matches++;
-      }
+      my $m = $matrix->[$r + $pair->[0][0]][$c + $pair->[0][1]];
+      my $s = $matrix->[$r + $pair->[1][0]][$c + $pair->[1][1]];
+      $matches++ if $m eq "M" && $s eq "S";
     }
 
-    if ($matches > 1) {
-      $part2++;
-    }
+    $part2++ if $matches >= 2;
   }
 }
 
