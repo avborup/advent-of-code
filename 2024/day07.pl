caac63f12e0d2e889ac6bff0ced476314e58299a
Adrian Borup <adrian@borup.com>
2024-12-07 11:46:20 +0100

    Clean up day 7

--- 2024/day07.pl
+++ 2024/day07.pl
@@ -1,25 +1,23 @@
 #!/usr/bin/env perl -w
 use strict;
 
-chomp(my @input = <>);
-
 my ($part1, $part2) = (0, 0);
-for my $line (@input) {
+for my $line (<>) {
   my ($goal, @args) = $line =~ /(\d+)/g;
   $part1 += $goal if brute($goal, $args[0], \@args, 1, 0);
   $part2 += $goal if brute($goal, $args[0], \@args, 1, 1);
 }
 
 sub brute {
-  my ($goal, $acc, $nums, $i, $or) = @_;
+  my ($goal, $acc, $nums, $i, $conc) = @_;
 
-  return 1 if $i > $#$nums && $goal == $acc;
-  return 0 if $i > $#$nums;
-
+  return $goal == $acc if $i > $#$nums;
   my $num = $nums->[$i];
-  my $valid = brute($goal, $acc + $num, $nums, $i + 1, $or) || brute($goal, $acc * $num, $nums, $i + 1, $or);
-  $valid ||= brute($goal, "$acc$num", $nums, $i + 1, $or) if $or;
-  return $valid;
+
+  my $found = brute($goal, $acc + $num, $nums, $i+1, $conc) || brute($goal, $acc * $num, $nums, $i+1, $conc);
+  $found ||= brute($goal, "$acc$num", $nums, $i+1, $conc) if $conc;
+
+  return $found;
 }
 
 print("Part 1: $part1\n");
