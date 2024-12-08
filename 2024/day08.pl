1d48310e6c3cb672c1e1d25edbfe57d326e57841
Adrian Borup <adrian@borup.com>
2024-12-08 07:18:57 +0100

    Clean up day 8 even more

--- 2024/day08.pl
+++ 2024/day08.pl
@@ -1,31 +1,33 @@
 #!/usr/bin/env perl -w
 use strict;
 
+use Math::Complex;
+
 my $grid = [map { chomp; [split //] } <>];
 my ($R, $C) = (scalar(@$grid), scalar(@{$grid->[0]}));
 
 my %antennas;
 for my $r (0..$R-1) {
   for my $c (0..$C-1) {
-    push @{$antennas{$grid->[$r][$c]}}, [$r, $c] if $grid->[$r][$c] ne ".";
+    push @{$antennas{$grid->[$r][$c]}}, cplx($r, $c) if $grid->[$r][$c] ne ".";
   }
 }
 
 sub add {
-  my ($into, $r, $c) = @_;
-  $into->{"$r,$c"} = 1 if $r >= 0 && $r < $R && $c >= 0 && $c < $C;
+  my ($into, $p) = @_;
+  $into->{$p} = 1 if Re($p) >= 0 && Re($p) < $R && Im($p) >= 0 && Im($p) < $C;
 }
 
 my (%part1, %part2);
-for $a (keys %antennas) {
-  for my $i (0..$#{$antennas{$a}}) {
-    for my $j ($i+1..$#{$antennas{$a}}) {
-      my ($r1, $c1, $r2, $c2) = (@{$antennas{$a}[$i]}, @{$antennas{$a}[$j]});
-      my ($dr, $dc) = ($r2 - $r1, $c2 - $c1);
+for my $antennas (values %antennas) {
+  for my $i (0..$#$antennas) {
+    for my $j ($i+1..$#$antennas) {
+      my ($a, $b) = ($antennas->[$i], $antennas->[$j]);
 
-      add(\%part1, $r2 + $dr, $c2 + $dc);
-      add(\%part1, $r2 - 2*$dr, $c2 - 2*$dc);
-      add(\%part2, $r2 + $_*$dr, $c2 + $_*$dc) for -$R..$R;
+      my $d = $a - $b;
+      add(\%part1, $a + $d);
+      add(\%part1, $a - 2*$d);
+      add(\%part2, $a + $_*$d) for -$R..$R;
     }
   }
 }
