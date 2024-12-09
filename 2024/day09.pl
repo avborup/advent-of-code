e240eba60bdf6a8bfd036c3e421287d51ab84667
Adrian Borup <adrian@borup.com>
2024-12-09 21:52:42 +0100

    Clean up day 9

--- 2024/day09.pl
+++ 2024/day09.pl
@@ -1,76 +1,59 @@
 #!/usr/bin/env perl -w
 use strict;
 
-use Data::Dumper;
-
-my ($part1, $part2) = (0, 0);
-
 my @input = split //, <>;
+my ($id, %files) = 0;
+$files{$id++} = {blocks => shift @input, free => shift @input // 0} while @input;
 
-my %files;
-my $id = 0;
-while (@input) {
-  $files{$id++} = {blocks => shift @input, free => shift @input // 0};
+print("Part 1: ", part_1(), "\n");
+print("Part 2: ", part_2(), "\n");
+
+sub part_1 {
+  my @disk;
+  for my $id (sort { $a <=> $b } keys %files) {
+    push @disk, ($id) x $files{$id}->{blocks};
+    push @disk, (".") x $files{$id}->{free}
+  }
+
+  my $checksum = 0;
+  for (my $i = 0; $i < @disk; $i++) {
+    pop @disk while $disk[-1] eq ".";
+    $disk[$i] = pop @disk if $disk[$i] eq ".";
+    $checksum += $i * $disk[$i];
+  }
+  # print @disk, "\n";
+
+  return $checksum;
 }
 
-# my @disk = ();
-my (@alloced, @free);
-my $pos = 0;
-for $id (sort { $a <=> $b } keys %files) {
-  # for my $i (1..$files{$id}->{blocks}) {
-  #   push @disk, $id;
-  # }
-  # for my $i (1..$files{$id}->{free}) {
-  #   push @disk, ".";
-  # }
+sub part_2 {
+  my ($pos, @alloced, @free) = (0);
+  for my $id (sort { $a <=> $b } keys %files) {
+    push @alloced, {pos => $pos, id => $id, size => $files{$id}->{blocks}};
+    $pos += $files{$id}->{blocks};
+    push @free, {pos => $pos, size => $files{$id}->{free}};
+    $pos += $files{$id}->{free};
+  }
 
-  push @alloced, {pos => $pos, id => $id, size => $files{$id}->{blocks}};
-  $pos += $files{$id}->{blocks};
-  push @free, {pos => $pos, size => $files{$id}->{free}};
-  $pos += $files{$id}->{free};
-}
+  for (my $b = $#alloced; $b >= 0; $b--) {
+    my $block = $alloced[$b];
 
-for (my $b = $#alloced; $b >= 0; $b--) {
-  my $block = $alloced[$b];
+    for my $f (@free) {
+      last if $f->{pos} >= $block->{pos};
 
-  # print "Moving file $block->{id} from $block->{pos} to ";
-
-  for my $f (@free) {
-    if ($f->{pos} >= $block->{pos}) {
-      last;
-    }
-
-    if ($f->{size} >= $block->{size}) {
-      $block->{pos} = $f->{pos};
-      $f->{pos} += $block->{size};
-      $f->{size} -= $block->{size};
-      last;
+      if ($f->{size} >= $block->{size}) {
+        $block->{pos} = $f->{pos};
+        $f->{pos} += $block->{size};
+        $f->{size} -= $block->{size};
+        last;
+      }
     }
   }
 
-  # print "$block->{pos}\n";
-}
-
-for my $block (sort { $a->{pos} <=> $b->{pos} } @alloced) {
-  my $pos = $block->{pos};
-  for my $i (0..$block->{size}-1) {
-    # print $pos+$i, " * $block->{id}\n";
-    $part2 += ($pos + $i) * $block->{id};
+  my $checksum = 0;
+  for my $block (@alloced) {
+    $checksum += ($block->{pos} + $_) * $block->{id} for 0..$block->{size}-1;
   }
+
+  return $checksum;
 }
-
-# my ($checksum, %seen) = (0);
-# for (my $i = 0; $i < @disk; $i++) {
-#   if ($disk[$i] eq ".") {
-#     pop @disk while $disk[-1] eq ".";
-#     $disk[$i] = pop @disk;
-#   }
-#   $checksum += $i * $disk[$i];
-# }
-# $part1 = $checksum;
-#
-#
-# print @disk, "\n";
-
-print("Part 1: $part1\n");
-print("Part 2: $part2\n");
