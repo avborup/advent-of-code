5d74e6166c37bc0bd8e8d1e696af64b14c604efa
Adrian Borup <adrian@borup.com>
2024-12-10 06:49:02 +0100

    Add day 10 part 2, cleaned

--- 2024/day10.pl
+++ 2024/day10.pl
@@ -1,75 +1,52 @@
 #!/usr/bin/env perl -w
 use strict;
 
-use Data::Dumper;
-use Math::Complex;
+use List::Util qw(sum);
 
 my $matrix = [map { [split //] } map { chomp; $_ } <>];
 
-my $part_1 = 0;
-my $part_2 = 0;
+my ($part1, $part2) = 0;
+for my $bottom (find_bottom_points()) {
+  my $tops = dfs($bottom);
+  $part1 += scalar keys %$tops;
+  $part2 += sum values %$tops;
+}
 
-my @lows = find_low_points($matrix);
+print("Part 1: $part1\n");
+print("Part 2: $part2\n");
 
-my $dfs = sub {
-  my %visited;
+sub dfs {
   my %tops;
-  my ($start) = @_;
-  my ($sr, $sc) = @$start;
-
-  my @stack = ($start);
-  $visited{cplx($sc, $sr)} = 1;
-
+  my @stack = @_;
   while (@stack) {
-    my $v = pop @stack;
-    my ($vr, $vc) = @$v;
+    my ($vr, $vc) = @{pop @stack};
 
-    if ($matrix->[$vr][$vc] == 9) {
-      $tops{"$vc,$vr"} = 1;
-    }
+    $tops{"$vr,$vc"} += 1 if $matrix->[$vr][$vc] == 9;
 
-    foreach my $adj (@{neighbours($vr, $vc, $matrix)}) {
+    for my $adj (@{neighbours($vr, $vc, $matrix)}) {
       my ($ar, $ac) = @$adj;
-
       next unless $matrix->[$ar][$ac] - $matrix->[$vr][$vc] == 1;
-
-      next if $visited{"$ar,$ac"};
-      $visited{"$ar,$ac"} = 1;
-
       push @stack, $adj;
     }
   }
 
-  return scalar keys %tops;
-};
-
-
-
-foreach my $low (@lows) {
-  my $res = $dfs->($low);
-  print "Low: @$low -> $res\n";
-  $part_1 += $res;
+  return \%tops;
 }
 
-print("Part 1: ", $part_1, "\n");
-print("Part 2: ", $part_2, "\n");
-
-sub find_low_points {
-  my ($matrix) = @_;
+sub find_bottom_points {
   my @lows;
   for my $r (0 .. $#$matrix) {
-      for my $c (0 .. $#{$matrix->[$r]}) {
-      my $height = $matrix->[$r][$c];
-      push @lows, [$r, $c] if $height == 0;
+    for my $c (0 .. $#{$matrix->[$r]}) {
+      push @lows, [$r, $c] if $matrix->[$r][$c] == 0;
     }
   }
   return @lows;
 }
 
 sub neighbours {
-  my ($x, $y, $matrix) = @_;
+  my ($x, $y) = @_;
   my ($R, $C, @neigbours) = ($#$matrix, $#{$matrix->[0]});
-  foreach my $dir ([-1, 0], [1, 0], [0, -1], [0, 1]) {
+  for my $dir ([-1, 0], [1, 0], [0, -1], [0, 1]) {
     my ($nx, $ny) = ($x + $dir->[0], $y + $dir->[1]);
     push @neigbours, [$nx, $ny] unless $nx < 0 || $nx > $R || $ny < 0 || $ny > $C;
   }
