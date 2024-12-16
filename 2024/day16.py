1ce531040a28a51142faf17d961c7d007a2bfc34
Adrian Borup <adrian@borup.com>
2024-12-16 07:30:15 +0100

    tmp i think it works, slow

--- 2024/day16.py
+++ 2024/day16.py
@@ -12,19 +12,36 @@ start = next(k for k, v in grid.items() if v == 'S')
 end = next(k for k, v in grid.items() if v == 'E')
 
 
+class Node:
+    def __init__(self, pos, dir, path, dist):
+        self.pos = pos
+        self.dir = dir
+        self.path = path
+        self.dist = dist
+
+    def __lt__(self, other):
+        return self.dist < other.dist
+
+
+    def __hash__(self):
+        return hash((self.pos, self.dir))
+
+
 def adj(v):
-    pos, dir = v
+    pos, dir, path = v.pos, v.dir, v.path
     reachable = []
 
     if grid[pos + dir] != '#':
-        reachable.append(((pos + dir, dir), 1))
+        reachable.append(((pos + dir, dir, path + [pos + dir]), 1))
 
     for rot in [1j, 1j ** 3]:
         new_dir = dir * rot
         if grid[pos + new_dir] != '#':
-            reachable.append(((pos + new_dir, new_dir), 1001))
+            reachable.append(((pos + new_dir, new_dir, path+[pos + new_dir]), 1001))
 
     return reachable
+
+
 
 
 def dijkstra(S, T):
@@ -35,16 +52,23 @@ def dijkstra(S, T):
     iters = 0
 
     def add(i, dst):
-        if dst < dist[i]:
-            dist[i] = dst
-            push(pq, (dst, iters, i))
+        n = Node(*i, dist=dst)
+        if dst < dist[n]:
+            dist[n] = dst
+            push(pq, n)
 
-    add((S, 1), 0)
+    add((S, 1, [S]), 0)
+    gucci = set()
+    min_dist = INF
 
     while pq:
-        D, _, i = pop(pq)
-        if i[0] == end:
-            return D
+        n = pop(pq)
+        D, i = n.dist, n
+        if i.pos == end:
+            if D > min_dist:
+                return len(gucci)
+            min_dist = D
+            gucci = gucci.union(i.path)
         if D != dist[i]:
             continue
         for j, w in adj(i):
