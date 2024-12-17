ddcc556f756046cf345ebb7071e6b87dea11a4e3
Adrian Borup <adrian@borup.com>
2024-12-17 09:19:22 +0100

    Clean up day 17

--- 2024/day17.py
+++ 2024/day17.py
@@ -24,6 +24,10 @@ def run_op(state, program):
 
     return ptr, A, B, C, output
 
+def run(state, program):
+    while state[0] < len(program):
+        state = run_op(state, program)
+    return state
 
 def pretty_program(prog):
     """
@@ -57,92 +61,28 @@ def pretty_program(prog):
         elif opcode == 6: print(f"B = A >> {combo}")
         elif opcode == 7: print(f"C = A >> {combo}")
 
-
-def run(state, program):
-    while state[0] < len(program):
-        state = run_op(state, program)
-    return state
-
-
 def solve(program, index, acc):
-    # bitgroups = []
-    # for i in range(len(program)):
-    #     for a in range(2 ** 3):
-    #         res = run((0, a, orig_B, orig_C, []), program)
-    #         if res[4] == [program[i]]:
-    #             bitgroups.append(a)
-    #             break
-    # a = 0
-    # for n in reversed(bitgroups):
-    #     a = (a << 3) + n
-
-    for group in range(2 ** 3):
-        a = (acc << 3) + group
+    for bit_group in range(2 ** 3):
+        a = (acc << 3) + bit_group
         res = run((0, a, orig_B, orig_C, []), program)
 
-        if res[4] == program[index:]:
-            if index <= 0:
-                return a
-
+        if res[4][0] == program[index]:
+            if index <= 0: return a
             if (rec := solve(program, index - 1, a)) is not None:
                 return rec
-
     return None
-
-
-
-    print(a, bitgroups)
 
 orig_A = int(next(stdin).split()[-1])
 orig_B = int(next(stdin).split()[-1])
 orig_C = int(next(stdin).split()[-1])
-
 next(stdin)
 program = [int(x) for x in next(stdin).lstrip("Program: ").strip().split(",")]
 
 pretty_program(program)
+print()
 
-print(run((0, orig_A, orig_B, orig_C, []), program))
+part1 = run((0, orig_A, orig_B, orig_C, []), program)
+part2 = solve(program, len(program) - 1, 0)
 
-print(solve(program, len(program) - 1, 0))
-
-
-# min A = 2**44
-# max A = 2**48
-
-outs = []
-
-# 1) min/max by length 16
-# 2) try randoms, reduce min/max ranges with most matching final elements
-
-# 235928149323145 : 5,3,5,1,4,4,5,6,6,5,2,5,5,5,3,0
-# 235931905437136 : 4,1,2,0,5,7,1,6,3,1,4,1,5,5,3,0
-# 235932903155938 : 5,0,6,0,5,1,1,5,3,1,3,1,5,5,3,0
-
-start = 531104
-stepsize = 262144
-microstep = 31
-
-# for i in range(2**45, 2**48):
-#     ptr = 0
-#     A = i
-#     B = orig_B
-#     C = orig_C
-#     output.clear()
-#     run()
-#
-#     match = 6
-#     if output[:match] == program[:match]:
-#         print(len(output), ":", ",".join(str(x) for x in output))
-#         print("Progress", i)
-#
-#     outs.append((i, output.copy()))
-#
-#     if program == output:
-#         print("Found", i)
-#         break
-
-# outs.sort(key=lambda x: x[0])
-# for i, out in outs:
-#     print(i, ":", ",".join(str(x) for x in out))
-
+print("Part 1:", ','.join(str(x) for x in part1[4]))
+print("Part 1:", part2)
