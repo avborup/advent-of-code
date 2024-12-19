849ec1770f5f00ee0e4aaf678d43232c279afc50
Adrian Borup <adrian@borup.com>
2024-12-19 06:21:14 +0100

    Clean up day 19 and include both parts

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,31 +1,26 @@
 from sys import stdin
-from collections import deque
 from functools import cache
 
 valid = next(stdin).strip().split(", ")
-next(stdin)
-designs = [l.strip() for l in stdin]
-
-part1, part2 = 0, 0
+designs = [l.strip() for l in stdin][1:]
 
 @cache
-def dfs(design):
+def solve(design):
     if len(design) == 0:
         return 1
 
-    tot = 0
+    num_options = 0
     for pattern in valid:
         if design.startswith(pattern):
-            new_design = design[len(pattern):]
-            tot += dfs(new_design)
-    return tot
+            num_options += solve(design[len(pattern):])
 
+    return num_options
 
-for i, design in enumerate(designs):
-    res = dfs(design)
-    print(i, design, res)
-    part1 += res
-
+part1, part2 = 0, 0
+for design in designs:
+    num_options = solve(design)
+    part1 += 1 if num_options > 0 else 0
+    part2 += num_options
 
 print("Part 1:", part1)
 print("Part 2:", part2)
