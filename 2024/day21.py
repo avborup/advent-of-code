404411dfa6363fcf5dc424a1ce4915150acdb257
Adrian Borup <adrian@borup.com>
2024-12-21 11:31:55 +0100

    Make good debug prints for helping out friend

--- 2024/day21.py
+++ 2024/day21.py
@@ -19,7 +19,6 @@ DIR = make_pad([
     ['<', 'v', '>'],
 ])
 
-@cache
 def find_presses_for_code(code, max_level, level=0, cur_pos=None):
     if not code:
         return 0, ""
@@ -27,38 +26,41 @@ def find_presses_for_code(code, max_level, level=0, cur_pos=None):
     pad = NUMERIC if level == 0 else DIR
     cur_pos = cur_pos if cur_pos is not None else pad['A']
 
-    target_key = code[0]
-    target_pos = pad[target_key]
+    total_cost = 0
+    total_option = ""
+    for i, target_key in enumerate(code):
+        target_pos = pad[target_key]
 
-    diff = target_pos - cur_pos
-    dr, dc = int(diff.real), int(diff.imag)
-    hor_sym = '>' if dc > 0 else '<'
-    ver_sym = 'v' if dr > 0 else '^'
+        diff = target_pos - cur_pos
+        dr, dc = int(diff.real), int(diff.imag)
+        hor_sym = '>' if dc > 0 else '<'
+        ver_sym = 'v' if dr > 0 else '^'
 
-    options = set()
-    if cur_pos + complex(0, dc) in pad.values():
-        options.add(hor_sym * abs(dc) + ver_sym * abs(dr))
-    if cur_pos + complex(dr, 0) in pad.values():
-        options.add(ver_sym * abs(dr) + hor_sym * abs(dc))
+        options = set()
+        if cur_pos + complex(0, dc) in pad.values():
+            options.add(hor_sym * abs(dc) + ver_sym * abs(dr))
+        if cur_pos + complex(dr, 0) in pad.values():
+            options.add(ver_sym * abs(dr) + hor_sym * abs(dc))
 
-    # print(level * '  ', f'{level})', code, f"{options}", "from", cur_pos)
+        from_key = next(k for k, v in pad.items() if v == cur_pos)
+        print(level * '  ', f'{level})', target_key, f"{options}", "from", from_key.__repr__())
 
-    if level < max_level:
-        def option_cost(option):
-            option = option + "A"
-            return find_presses_for_code(option, max_level, level + 1)
+        if level < max_level:
+            def option_cost(option):
+                option = option + "A"
+                return find_presses_for_code(option, max_level, level + 1)
 
-        cost, option = min(option_cost(option) for option in options)
-    else:
-        opt = next(iter(options)) + "A"
-        cost, option = len(opt), opt
+            min_cost, min_option = min(option_cost(option) for option in options)
+        else:
+            opt = next(iter(options)) + "A"
+            min_cost, min_option = len(opt), opt
 
-    subcost, suboption = find_presses_for_code(code[1:], max_level, level, target_pos)
+        total_cost += min_cost
+        total_option += min_option
 
-    total_cost = cost + subcost
-    total_option = option # + suboption
+        cur_pos = target_pos
 
-    # print(level * '  ', ".", code, "=", f"({cost} + {subcost})", "presses for", total_option)
+        print(level * '  ', ".", code[:i+1], "=", f"{total_cost} (+{min_cost})", "presses for", total_option)
 
     return total_cost, total_option
 
@@ -68,7 +70,8 @@ part1, part2 = 0, 0
 for code in codes:
     num_code = int(code[:-1])
     part1 += find_presses_for_code(code, max_level=2)[0] * num_code
-    part2 += find_presses_for_code(code, max_level=25)[0] * num_code
+    print()
+    # part2 += find_presses_for_code(code, max_level=25)[0] * num_code
 
 print("Part 1:", part1)
 print("Part 2:", part2)
