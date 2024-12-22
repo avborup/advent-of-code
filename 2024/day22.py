1883c61137c8c949f7005a972cb0a0fbebb46b4f
Adrian Borup <adrian@borup.com>
2024-12-22 06:57:12 +0100

    Add day 22 part 2

--- 2024/day22.py
+++ 2024/day22.py
@@ -1,4 +1,5 @@
 from sys import stdin
+from collections import defaultdict
 
 def evolve(secret):
     MOD = 16777216
@@ -9,11 +10,26 @@ def evolve(secret):
 
 nums = [int(line) for line in stdin]
 
-part1, part2 = 0, 0
+part1 = 0
+all_sequences = defaultdict(int)
 for secret in nums:
+    prev, diffs = secret % 10, []
     for _ in range(2000):
         secret = evolve(secret)
+        price = secret % 10
+        diffs.append((price, price - prev))
+        prev = price
+
     part1 += secret
+
+    sequences = set()
+    for i in range(len(diffs) - 4):
+        prices, seq = zip(*diffs[i:i + 4])
+        if seq not in sequences:
+            sequences.add(seq)
+            all_sequences[seq] += prices[-1]
+
+part2 = max(all_sequences.values())
 
 print("Part 1:", part1)
 print("Part 2:", part2)
