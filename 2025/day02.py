c0ed23b6e022bb86cc2c0952a3bb98144a7abc56
Adrian Borup <adrian@borup.com>
2025-12-02 07:04:49 +0100

    Add day 2, part 2

--- 2025/day02.py
+++ 2025/day02.py
@@ -6,12 +6,18 @@ part2 = 0
 for start, end in ranges:
     for n in range(start, end + 1):
         s = str(n)
+
         if s[:len(s)//2] == s[len(s)//2:]:
             part1 += n
-            print(n)
+            part2 += n
+        else:
+            for i in range(1, len(s)//2+1):
+                base = s[:i]
 
+                if s.count(base) * len(base) == len(s):
+                    part2 += n
+                    break
 
-print(ranges)
 
 print("Part 1:", part1)
 print("Part 2:", part2)
