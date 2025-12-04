1ff62c873272a4da5b5a24920cb40be43efa7979
Adrian Borup <adrian@borup.com>
2025-12-04 06:43:21 +0100

    Add day 4 part 2

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,40 +1,33 @@
 from sys import stdin
 
-grid = [line.strip() for line in stdin]
+grid = [list(line.strip()) for line in stdin]
 R, C = len(grid), len(grid[0])
 
 part1, part2 = 0, 0
-lolleren = set()
+changed = True
+first = True
 
-for r in range(R):
-    for c in range(C):
-        diffs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        num_neighbors = 0
-        for dr, dc in diffs:
-            rr, cc = (r + dr, c + dc)
-            if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == '@':
-                num_neighbors += 1
-        if grid[r][c] == '@' and num_neighbors < 4:
-            lolleren.add((r, c))
-            part1 += 1
+while changed:
+    changed = False
+    new_grid = [[col for col in row] for row in grid]
 
-for r in range(R):
-    for c in range(C):
-        if (r, c) in lolleren:
-            print("X", end="")
-        else:
-            print(grid[r][c], end="")
-    print()
+    for r in range(R):
+        for c in range(C):
+            diffs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+            num_neighbors = 0
+            for dr, dc in diffs:
+                rr, cc = (r + dr, c + dc)
+                if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == '@':
+                    num_neighbors += 1
+            if grid[r][c] == '@' and num_neighbors < 4:
+                new_grid[r][c] = 'x'
+                if first:
+                    part1 += 1
+                part2 += 1
+                changed = True
+
+    grid = new_grid
+    first = False
 
 print("Part 1:", part1)
-
-# ..xx.xx@x.
-# x@@.@.@.@@
-# @@@@@.x.@@
-# @.@@@@..@.
-# x@.@@@@.@x
-# .@@@@@@@.@
-# .@.@.@.@@@
-# x.@@@.@@@@
-# .@@@@@@@@.
-# x.x.@@@.x.
+print("Part 2:", part2)
