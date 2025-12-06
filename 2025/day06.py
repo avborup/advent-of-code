d0e5e168f63f2ed88f5557e6e0a4523da9f19d0c
Adrian Borup <adrian@borup.com>
2025-12-06 07:51:05 +0100

    Add day 6 part 2

--- 2025/day06.py
+++ 2025/day06.py
@@ -1,7 +1,10 @@
+from functools import reduce
+from operator import mul
 from sys import stdin
 
+lines = [line for line in stdin]
 grid = []
-for line in stdin:
+for line in lines:
     items = [i for i in line.strip().split() if len(i) > 0]
     grid.append(items)
 
@@ -15,7 +18,33 @@ for col in range(len(grid[0])):
             res += cell
         elif operator == "*":
             res *= cell
-
     part1 += res
 
-print(part1)
+c = 0
+part2 = 0
+for _ in range(len(grid[0])):
+    operator = grid[-1][_]
+
+    until = c
+    for d in range(c+1, len(lines[0])):
+        if lines[-1][d] != " ":
+            until = d + (lines[-1][d] == "\n")
+            break
+
+    nums = []
+    for cc in range(c, until-1):
+        digits = []
+        for row in range(len(lines)-1):
+            digits.append(lines[row][cc])
+        num = int(''.join(digits))
+        nums.append(num)
+
+    if operator == "+":
+        part2 += sum(nums)
+    else:
+        part2 += reduce(mul, nums, 1)
+
+    c = until
+
+print("Part 1:", part1)
+print("Part 2:", part2)
