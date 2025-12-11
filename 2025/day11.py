ff42b187f32284ccb5c41cd3879cb0882495c6a9
Adrian Borup <adrian@borup.com>
2025-12-11 07:37:42 +0100

    Solve both parts in 1 file

--- 2025/day11.py
+++ 2025/day11.py
@@ -3,18 +3,16 @@ from sys import stdin
 from functools import cache
 
 graph = defaultdict(list)
-
 for line in stdin:
     k, vals = line.strip().split(": ")
     graph[k] = vals.split()
 
-print(graph)
-
 @cache
-def dfs(v, dac, fft):
-    if v == "out" and dac and fft:
+def dfs(v, dac, fft, part2):
+    if v == "out" and (not part2 or dac and fft):
         return 1
 
-    return sum(dfs(n, dac or (n == "dac"), fft or (n == "fft")) for n in graph[v])
+    return sum(dfs(n, dac or (n == "dac"), fft or (n == "fft"), part2) for n in graph[v])
 
-print("Part 2:", dfs("svr", False, False))
+print("Part 1:", dfs("you", False, False, False))
+print("Part 2:", dfs("svr", False, False, True))
